0669ec4b7af0fb80c046f27f4ef2286ad4ec774c
monsegonzales <monseeglzg@gmail.com>
2025-12-09 04:22:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,2 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.tree import DecisionTreeClassifier
-st.set_page_config(
-    page_title="¿Estás en forma? - Clasificador",
-    page_icon="🏃‍♀️",
-    layout="centered"
